cfb8fd70a46510ed654436f0a25de388c393e873
Oscar Pacheco <Oscarpa88@yahoo.com>
2021-01-08 02:54:34 -0600

    nasa data retrieved

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
+import Nasa from './views/nasa_component.js'
 import "./App.css";
 
+// NASA API https://api.nasa.gov/planetary/apod?api_key=xRpeUnyvEDRmAApcSHEUfqjg6naHJUW8raV71s8D
+
 function App() {
+
+  const [nasaImg, setNasaImg] = useState({})
+
+  useEffect(()=> {
+    const nasaPhoto = axios.get('https://api.nasa.gov/planetary/apod?api_key=xRpeUnyvEDRmAApcSHEUfqjg6naHJUW8raV71s8D').then(res => {
+    setNasaImg(res.data)
+    }).catch(err => console.log(err))
+
+  }, [])
+
+  console.log(nasaImg)
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <Nasa />
     </div>
   );
 }
---
+++ src/views/nasa_component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Nasa = () => {
+    return(
+        <div> 
+           Nasa Component 
+        </div>
+    )
+}
+
+
+export default Nasa;
